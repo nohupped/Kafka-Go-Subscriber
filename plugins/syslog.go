36d9da86cbacb222c0427a29d4586db3ad5a941e
Girish <nohupped@gmail.com>
2017-03-07 22:10:50 +0530

    goroutines working as expected and some parts separated

--- plugins/syslog.go
+++ plugins/syslog.go
@@ -2,19 +2,29 @@ package plugins
 
 import (
 	"github.com/Shopify/sarama"
-	"fmt"
-	"os"
+	//"fmt"
+	//"os"
 	"github.com/bsm/sarama-cluster"
 	log "github.com/nohupped/glog"
+//	"github.com/jeromer/syslogparser"
+//	"github.com/jeromer/syslogparser/rfc3164"
+//	"github.com/jeromer/syslogparser/rfc5424"
+
 
 	"time"
+//	"fmt"
+	//"os"
+	"encoding/json"
+	"github.com/jeromer/syslogparser/rfc5424"
+	"fmt"
 )
 
 func PluginSyslog(messages chan *sarama.ConsumerMessage, consumer *cluster.Consumer, logger *log.Logger) {
 	logger.Infoln("Started plugin syslog...")
 	// Ranging over the channel messages.
 	for msg := range messages {
-		fmt.Fprintf(os.Stdout, "From Plugin, %s/%d/%d\n\t%s\n\t%s\n", msg.Topic, msg.Partition, msg.Offset, msg.Key, msg.Value)
+		//fmt.Fprintf(os.Stdout, "From Plugin, %s/%d/%d\n\t%s\n\t%s\n", msg.Topic, msg.Partition, msg.Offset, msg.Key, msg.Value)
+		ParseSyslog(msg.Value)
 		consumer.MarkOffset(msg, "")
 	}
 	// Once the consumer is closed upon receiving an interrupt, the range over the channel will be finished, and the below wg.Done
@@ -24,4 +34,24 @@ func PluginSyslog(messages chan *sarama.ConsumerMessage, consumer *cluster.Consu
 		logger.Infoln(i)
 		time.Sleep(time.Second * 1)
 	}
+}
+
+func ParseSyslog(msg []byte) {
+	//fmt.Fprintf(os.Stdout, "%s\n", msg)
+	data := make(map[string]interface{})
+	err := json.Unmarshal(msg, &data)
+	if err != nil {
+		panic(err)
+	}
+	strippedMsg := []byte(data["message"].(string))
+	p1 := rfc5424.NewParser(strippedMsg)
+	p1.Parse()
+	fmt.Println(p1.Dump())
+	/*	p1 := rfc5424.NewParser(msg)
+	p2 := rfc3164.NewParser(msg)
+	p1.Parse()
+	p2.Parse()
+	fmt.Println("From rfc5424", p1.Dump())
+	fmt.Println("From rfc3164", p2.Dump())
+	*/
 }
