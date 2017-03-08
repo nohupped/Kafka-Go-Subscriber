a4134b558c9ab2c83aaf09171cfd8e869619340f
Girish <nohupped@gmail.com>
2017-03-08 22:14:50 +0530

    syslog plugin initial phase working.

--- plugins/syslog.go
+++ plugins/syslog.go
@@ -9,7 +9,6 @@ import (
 
 	"time"
 	"net"
-	"fmt"
 	"bytes"
 
 )
@@ -17,18 +16,28 @@ import (
 // RFC syslog format: <13>2017-03-08T12:29:02.231335+00:00 openldap1 slapd[392]: slap_global_control: unrecognized control: 1.3.6.1.4.1.42.2.27.8.5.1
 // Construct a log of similar format IF it is just a file tail from syslog or similar logs.
 func StartPluginSyslog(messages chan *sarama.ConsumerMessage, consumer *cluster.Consumer, logger *log.Logger) {
-	DialSyslogServer("tcp", "172.24.40.36:514", time.Second * 10)
 	logger.Infoln("Started plugin syslog...")
+
+	DialSyslogServer("tcp", "172.19.116.144:514", time.Second * 10)
 	// Ranging over the channel messages.
+
+	Out:
 	for msg := range messages {
 		//fmt.Fprintf(os.Stdout, "From Plugin, %s/%d/%d\n\t%s\n\t%s\n", msg.Topic, msg.Partition, msg.Offset, msg.Key, msg.Value)
 		//strippedMsg := ParseSyslog(msg.Value)
+
 		parsedMsg := ParseSyslog(msg.Value)
-		written, err := conn.Write(parsedMsg)
-		if err != nil {
-			fmt.Println(err)
+		var written int
+		var WriteError error
+		written, WriteError = conn.Write(parsedMsg)
+		if WriteError != nil {
+			DialSyslogServer("tcp", "172.19.116.144:514", time.Second * 10)
+			written, WriteError = conn.Write(parsedMsg)
+			if WriteError != nil {
+				logger.Errorln("Cannor write message to socket, re-connection failed, plugin syslog dying...")
+				break Out
+			}
 		}
-		fmt.Println(written, "bytes written for the message", string(parsedMsg))
 		//if strippedMsg != nil {
 		//	written, err := conn.Write(strippedMsg)
 
@@ -40,6 +49,7 @@ func StartPluginSyslog(messages chan *sarama.ConsumerMessage, consumer *cluster.
 		//}
 //		fmt.Println(string(strippedMsg))
 		consumer.MarkOffset(msg, "")
+		logger.Debugln(written, "bytes written for the message", string(parsedMsg), "and marked consumer offset")
 
 	}
 	// Once the consumer is closed upon receiving an interrupt, the range over the channel will be finished, and the below wg.Done
